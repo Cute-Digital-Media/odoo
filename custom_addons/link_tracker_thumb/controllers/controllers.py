b29519955ae179f02a966a7e3c1f0859923f30a7
Jesus <cutedigitaldeveloper@gmail.com>
2025-08-29 13:11:06 -0500

    feat: :alembic: Test for log errors

--- custom_addons/link_tracker_thumb/controllers/controllers.py
+++ custom_addons/link_tracker_thumb/controllers/controllers.py
@@ -1,40 +1,35 @@
+import logging
 from odoo import http
 from odoo.http import request
-
-import logging
 
 _logger = logging.getLogger(__name__)
 
 
 class LinkTrackerController(http.Controller):
 
-    @http.route('/rt/<string:code>', type='http', auth='public', website=True)
-    def redirect_short_link(self, code, **kwargs):
+    @http.route('/rt/<string:shortcode>', type='http', auth='public')
+    def redirect_short_link(self, shortcode, **kwargs):
+        _logger.info("Incoming request with shortcode: %s", shortcode)
 
-        _logger.info("Incoming request with shortcode: %s", code)
-        _logger.debug("Request headers: %s", request.httprequest.headers)
-
-        tracker = request.env['link.tracker'].sudo().search([('code', '=', code)], limit=1)
+        # Odoo's link.tracker uses field `code` for the short code
+        tracker = request.env['link.tracker'].sudo().search([('code', '=', shortcode)], limit=1)
         if not tracker:
-            _logger.warning("No link found for shortcode: %s", code)
+            _logger.warning("No tracker found for shortcode: %s", shortcode)
             return request.not_found()
 
         user_agent = request.httprequest.headers.get('User-Agent', '').lower()
-
         _logger.info("User-Agent detected: %s", user_agent)
 
-        is_bot = any(bot in user_agent for bot in ['facebook', 'twitterbot', 'slackbot', 'whatsapp'])
+        crawler_signatures = ["facebookexternalhit", "twitterbot", "whatsapp", "telegrambot", "linkedinbot"]
 
-        if is_bot:
-            _logger.info("Crawler detected, rendering preview for shortcode %s", code)
-            values = {
-                'title': tracker.page_title or tracker.url,
-                'description': tracker.extended_description or '',
-                'image': tracker.thumbnail_url or '',
-                'url': tracker.url,
-            }
-            return request.render('link_tracker_thumb.link_tracker_preview', values)
-
-        # Regular users: redirect
-        _logger.info("Normal user detected, redirecting to: %s", tracker.url.target_url)
-        return request.redirect(tracker.url, code=302)
+        if any(sig in user_agent for sig in crawler_signatures):
+            _logger.info("Crawler detected, serving preview page")
+            return request.render("link_tracker_thumb.link_tracker_preview", {
+                "title": tracker.title or "",
+                "extended_description": tracker.extended_description or "",
+                "thumbnail_url": tracker.thumbnail_url or "",
+                "url": tracker.url or "",
+            })
+        else:
+            _logger.info("Normal user detected, redirecting to: %s", tracker.url)
+            return request.redirect(tracker.url, code=302)
